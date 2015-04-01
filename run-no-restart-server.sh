3d604f8b2eb9fbe8448925fdf36f4c9e81e9b4a1
JJ Merelo <jjmerelo@gmail.com>
2015-04-01 12:18:58 +0200

    Cleaning up branch

--- run-no-restart-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-foreman start web &
-for i in {1..50}; do
-    ( cd client; ./client-ea.js )
-done
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-for i in {1..50}; do
-    foreman start web & pid=$! && echo $pid
-    ( cd client; ./client-ea.js ) && kill $pid 
-done

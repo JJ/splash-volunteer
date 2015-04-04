e4d310c7fec226e699296e1a3038016ad3603112
JJ Merelo <jjmerelo@gmail.com>
2015-04-04 10:19:53 +0200

    Creates dir to avoid errors in heroku

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.set('port', (process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 5555));
 app.set('trust proxy', true );
 var log_dir = process.env.OPENSHIFT_DATA_DIR || "log";
+if (!fs.existsSync(log_dir)){
+    fs.mkdirSync(log_dir);
+}
 // set up static dir
 app.use(express.static(__dirname + '/public'))
 

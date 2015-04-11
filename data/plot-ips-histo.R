c56438c95da6bb8cc926ae599367fcbc069fd726
JJ Merelo <jjmerelo@gmail.com>
2015-04-11 20:10:35 +0200

    Processing new data

---
+++ data/plot-ips-histo.R
@@ -0,0 +1,4 @@
+library(ggplot2)
+ips <- read.csv("ips-per-run.dat",header=FALSE)
+ggplot(ips, aes(x=V1)) + geom_histogram( position="identity", binwidth=1)
+
--- data/plot-times.R
+++ data/plot-times.R
@@ -5,3 +5,4 @@ trap.base.times.restart <- read.csv("50runs.dat",header=FALSE)
 trap.base <- data.frame( restart=c(rep('Y',length(trap.base.times.restart$V1)),rep('N',length(trap.base.times.no.restart$V1))),
                         times=c(trap.base.times.restart$V1,trap.base.times.no.restart$V1) )
 ggplot(trap.base, aes(x=times, fill=restart)) + geom_histogram(alpha=0.2, position="identity")
+

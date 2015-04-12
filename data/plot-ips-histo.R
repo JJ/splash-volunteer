d298c471a830b94288bfad7d43f8fdac639667dd
JJ Merelo <jjmerelo@gmail.com>
2015-04-12 09:43:20 +0200

    Making stuff run as script

--- data/plot-ips-histo.R
+++ data/plot-ips-histo.R
@@ -1,4 +1,6 @@
+#!/usr/bin/env Rscript
+
 library(ggplot2)
 ips <- read.csv("ips-per-run.dat",header=FALSE)
 ggplot(ips, aes(x=V1)) + geom_histogram( position="identity", binwidth=1)
-
+ggsave("ips-per-run.png")

17780a5d755c743c03d7b4bbbb2ba30b363c0503
JJ Merelo <jjmerelo@gmail.com>
2015-07-01 20:16:10 +0200

    Plotting IPs for the first cartones experiment

--- data/plot-all-ips-histo.R
+++ data/plot-all-ips-histo.R
@@ -2,5 +2,5 @@
 
 library(ggplot2)
 ips <- read.csv("cartones-exp-1.csv")
-ggplot(ips, aes(x=reorder(IP,-puts),y=puts)) + geom_point()
+ggplot(ips, aes(x=reorder(IP,-puts),y=puts)) + geom_point() + scale_y_log10()
 ggsave("all-ips.png")

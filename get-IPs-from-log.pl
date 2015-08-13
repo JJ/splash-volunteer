8b2f9ca5563d60647272ed8ab23f229a2dc19c97
JJ Merelo <jjmerelo@gmail.com>
2015-08-13 12:04:12 +0200

    Adding puts per experiment

--- get-IPs-from-log.pl
+++ get-IPs-from-log.pl
@@ -27,13 +27,15 @@ while (@brackets ) {
     my $this_IP = shift @brackets;
     next if $this_IP =~ /start/;
     my %these_IPs;
+    my $puts = 0;
     while ( $this_IP !~ /solution/ ) {
-      my $msg_start = decode_json $this_IP;
-      my $this_ID = $msg_start->{'worker_uuid'}?$msg_start->{'worker_uuid'}:$msg_start->{'IP'};
-      $these_IPs{ $this_ID }++;
-      last if !@brackets;
-      $this_IP = shift @brackets;
-    }
+	$puts++;
+	my $msg_start = decode_json $this_IP;
+	my $this_ID = $msg_start->{'worker_uuid'}?$msg_start->{'worker_uuid'}:$msg_start->{'IP'};
+	$these_IPs{ $this_ID }++;
+	last if !@brackets;
+	$this_IP = shift @brackets;
+      }
     if ( $this_IP =~ /solution/ ) { #Maybe unpaired
       my $end = $this_IP;
       my $contents_end = decode_json $end;
@@ -42,9 +44,9 @@ while (@brackets ) {
 	- $format->parse_datetime( $contents_start->{'timestamp'} );
       push @times, 
 	[ scalar keys %these_IPs, 
-	  $duration->in_units('minutes')*60000+$duration->in_units('nanoseconds')/1e6 ]; #milliseconds
+	  $duration->in_units('minutes')*60000+$duration->in_units('nanoseconds')/1e6, $puts ]; #milliseconds
     }
 }
 
-say "IPs,milliseconds";
-say join("\n", map("$_->[0],$_->[1]",@times));
+say "IPs,milliseconds,PUTs";
+say join("\n", map("$_->[0],$_->[1],$_->[2]",@times));

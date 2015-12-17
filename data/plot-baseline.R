4a3798cf2c96dc2904402108caf52b9ace66ce8d
JJ Merelo <jjmerelo@gmail.com>
2015-12-17 18:24:44 +0100

    Changes graph for JJ/modeling-volunteer-computing#81

--- data/plot-baseline.R
+++ data/plot-baseline.R
@@ -2,5 +2,5 @@ library(ggplot2)
 
 duration.df <- data.frame(population=c(rep("512",length(baseline.duration$V1)),rep("1024",length(duration.1k$V1))),times=c(baseline.duration$V1,duration.1k$V1))
 
-ggplot(duration.df,aes(x=population,y=times))+geom_boxplot(notch=T)+scale_y_log10()+theme(axis.text=element_text(size=16), axis.title=element_text(size=18,face="bold"))
+ggplot(duration.df,aes(x=population,y=times))+geom_boxplot(notch=T)+scale_y_log10()+ ylab("Time (s)") + theme(axis.text=element_text(size=14), axis.title=element_text(size=16,face="bold"))
 ggsave("baseline-times.png",width=4,height=3)
